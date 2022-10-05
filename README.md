9a3a44de507bb8f289ae59b693f30d42f31dbf1f
PB12203006 <hm2689@columbia.edu>
2022-10-04 21:02:00 -0500

    post deployment

--- README.md
+++ README.md
@@ -4,8 +4,18 @@ We engraved our wedding information on Ethereum Mainnet
 
 ### Read Contract Data on Etherscan
 
+https://etherscan.io/address/0xac94D0e9D1C9ECE5EB8D943409Feb4624909fE9B
+
 ### Read Contract Data on Terminal
 
-1. Go to the repository root directory
-2. `npm i` ( with node 16 )
-3. `node read.js`
+##### Prerequisites
+
+We need a provider, I used Infura https://infura.io/dashboard
+
+##### On Terminal
+
+1. Create a `.env` file under repository root directory
+2. add this line to `.env` file : `PROVIDER_URL=<YOUR PROVIDER URL>`, no quote needed
+3. Go to the repository root directory
+4. `npm i` ( with node 16 )
+5. `node read.js`
--- read.js
+++ read.js
@@ -1,16 +1,15 @@
-import HDWalletProvider from "@truffle/hdwallet-provider";
 import Web3 from "web3";
 import dotenv from "dotenv";
 import abi, { ARRAY_FIELD_LENGTHS } from "./abi.js";
 
 dotenv.config();
-const provider = new HDWalletProvider(
-  [process.env.PRIVATE_KEY],
-  process.env.PROVIDER_URL
-);
+const provider = new Web3.providers.HttpProvider(process.env.PROVIDER_URL);
 const web3 = new Web3(provider);
 
-const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
+const contract = new web3.eth.Contract(
+  abi,
+  "0xac94D0e9D1C9ECE5EB8D943409Feb4624909fE9B"
+);
 
 async function main() {
   for (let method of abi) {
